85c7c872b0a44cb553814e712595b7f0f5f36a22
Shahria Abeed <shahriaabeed@gmail.com>
2024-07-02 14:01:26 -0400

    updated log messages in db-connect.ts

--- server/src/utils/db-connect.ts
+++ server/src/utils/db-connect.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectionStates } from "mongoose";
 import logger from './logger';
 require('dotenv').config();
 
@@ -11,16 +11,28 @@ async function db_connect() {
     if(mongoose.connection.readyState === 0) {
         logger.info('Connecting to DB because it is disconnected');
 
+        // check CLOUD_MONGO env var
+        // if 1 use cloud string
+        // if 0 or undefined use local
         if(process.env.CLOUD_MONGO !== undefined && process.env.CLOUD_MONGO === '1') {
             logger.info('Connecting to CLOUD database.');
             await mongoose.connect(`${process.env.CLOUD_STRING}`);
         }
         else {
+            if(process.env.CLOUD_MONGO === undefined) {
+                logger.warn('CLOUD_MONGO not defined in environment variables.');
+            }
+
             logger.info(`Connecting to LOCAL database.`);
             await mongoose.connect(LOCAL_STRING);
         }
 
-        logger.info(`mongoose.readyState: ${mongoose.connection.readyState}`)
+        if(mongoose.connection.readyState === 0) {
+            logger.error(`Failed to connect to db`);
+        }
+        else {
+            logger.info(`readyState: ${mongoose.connection.readyState}`);
+        }
     }
 }
 
