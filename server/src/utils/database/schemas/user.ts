ab9e15772ca364506e3e9afa40ee0bdbe1d1c3cd
Shahria Abeed <shahriaabeed@gmail.com>
2024-07-02 19:51:21 -0400

    added user schemas

---
+++ server/src/utils/database/schemas/user.ts
@@ -0,0 +1,16 @@
+import { Schema, Document } from "mongoose";
+
+
+export interface user extends Document {
+    email: string;
+    username: string;
+    password: string;
+    creationDate: Date;
+}
+
+export const userSchema = new Schema<user>({
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    creationDate: { type: Date, default: Date.now },
+});
