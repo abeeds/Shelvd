bbfdfce370295ca18a7237c305d10bd714705207
Shahria Abeed <shahriaabeed@gmail.com>
2024-07-02 14:15:54 -0400

    added try catch statements so app doesn't crash if db connect fails

--- server/src/utils/db-connect.ts
+++ server/src/utils/db-connect.ts
@@ -10,28 +10,33 @@ const LOCAL_STRING = "mongodb://localhost:27017";
 export default async function db_connect() {
     if(mongoose.connection.readyState === 0) {
         logger.info('Connecting to DB because it is disconnected');
+        var connection_string:string;
 
-        // check CLOUD_MONGO env var
-        // if 1 use cloud string
-        // if 0 or undefined use local
+        // define the connection_string by checking CLOUD_MONGO
+        // if 0 or undefined: local
+        // if 1: cloud
         if(process.env.CLOUD_MONGO !== undefined && process.env.CLOUD_MONGO === '1') {
             logger.info('Connecting to CLOUD database.');
-            await mongoose.connect(`${process.env.CLOUD_STRING}`);
+            connection_string = `${process.env.CLOUD_STRING}`;
         }
         else {
             if(process.env.CLOUD_MONGO === undefined) {
                 logger.warn('CLOUD_MONGO not defined in environment variables.');
             }
 
-            logger.info(`Connecting to LOCAL database.`);
-            await mongoose.connect(LOCAL_STRING);
+            logger.info('Connecting to LOCAL database.');
+            connection_string = LOCAL_STRING;
         }
 
-        if(mongoose.connection.readyState === 0) {
-            logger.error(`Failed to connect to db`);
+        try{
+            await mongoose.connect(connection_string);
         }
-        else {
-            logger.info(`readyState: ${mongoose.connection.readyState}`);
+        catch {
+            logger.error('Failed to connect to DB.');
+            logger.error('Please check that the DB is running '
+                + 'and the provided connection strings are correct.')
         }
+
+        logger.info(`readyState: ${mongoose.connection.readyState}`);
     }
 }
