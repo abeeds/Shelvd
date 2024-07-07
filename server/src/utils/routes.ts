2dab29b3a5937ba42a3d304d1bff80a672819b09
Shahria Abeed <shahriaabeed@gmail.com>
2024-07-07 17:04:06 -0400

    test endpoint to see if JSON works

--- server/src/utils/routes.ts
+++ server/src/utils/routes.ts
@@ -1,10 +1,24 @@
 import { Express, Request, Response } from "express";
+import { insertUser } from "./database/db-users";
+import logger from "./logger";
 
 
 function routes(app: Express) {
-    app.get('/test', (req: Request, res: Response) => {
+    app.get('/test/hello_world', (req: Request, res: Response) => {
+        logger.info(`${req.socket.remoteAddress} is calling /test/hello_world`);
+        
         res.status(200).send('Hello World!');
-    })    
+    })
+
+    app.post('/test/user', (req: Request, res: Response) => {
+        logger.info(`${req.socket.remoteAddress} is calling /test/user`);
+        const username: string = req.body.username;
+        const email: string = req.body.email;
+        const password: string = req.body.password;
+
+        logger.info(`email: ${email}, username: ${username}, password: ${password}`)
+        res.status(200).send();
+    })
 }
 
 
