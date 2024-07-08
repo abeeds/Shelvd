09678b90362c912a348e8a83df34c7f12ba8d9b0
Shahria Abeed <shahriaabeed@gmail.com>
2024-07-08 17:45:29 -0400

    updated the POST method to insert user and some early GET code

--- server/src/utils/routes.ts
+++ server/src/utils/routes.ts
@@ -1,30 +1,55 @@
 import { Express, Request, Response } from "express";
-import { insertUser } from "./database/db-users";
+import mongoose from "mongoose";
+import { getUsers, insertUser } from "./database/db-users";
 import logger from "./logger";
 
 
 function routes(app: Express) {
     app.get('/test/hello_world', (req: Request, res: Response) => {
         logger.info(`${req.socket.remoteAddress}: /test/hello_world`);
-        
+
         res.status(200).send('Hello World!');
     })
 
 
     // test/user
-    app.get('/test/user'), (req: Request, res: Response) => {
-        
-    }
+    app.get('/test/user', (req: Request, res: Response) => {
+        logger.info(`[${req.socket.remoteAddress}] [GET] [/test/user]`);
+        res.status(200).send(
+            getUsers()
+        )
+    })
 
-
+    /*
+        TODO
+        return error if username/email are already taken
+    */
     app.post('/test/user', (req: Request, res: Response) => {
-        logger.info(`${req.socket.remoteAddress}: /test/user`);
+        logger.info(`[${req.socket.remoteAddress}] [POST] [/test/user]`);
         const username: string = req.body.username;
         const email: string = req.body.email;
         const password: string = req.body.password;
+        
+        if(mongoose.connection.readyState !== 1) {
+            logger.error(`[${req.socket.remoteAddress}] [POST] [/test/user]: `
+                + `Failed to connect to the database.`);
 
-        logger.info(`email: ${email}, username: ${username}, password: ${password}`)
-        res.status(200).send();
+            res.status(500).send(`Failed to connect to the database.`);
+            return;
+        }
+
+        if(insertUser(username, email, password) !== null) {
+            logger.info(`[${req.socket.remoteAddress}] [POST] [/test/user]: `
+                + `Created user successfully.`);
+
+            res.status(200).send();
+        }
+        else {
+            logger.error(`[${req.socket.remoteAddress}] [POST] [/test/user]: `
+                + `Could not insert user`);
+
+            res.status(500).send(`Something went wrong.`)
+        }
     })
 }
 
