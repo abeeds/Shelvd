03b246e90a027ab70cce69305d597ca036fb4a8e
Shahria Abeed <shahriaabeed@gmail.com>
2024-07-15 19:15:38 -0400

    wrote test for updateOne and removed pointless .exec()'s from queries

--- server/src/tests/database/db-connect.test.ts
+++ server/src/tests/database/db-connect.test.ts
@@ -2,13 +2,14 @@ import assert from "node:assert";
 import test from "node:test";
 import { describe, after } from "node:test";
 import { userSchema } from "../../middleware/database/schemas/user";
-import { dbConnect, insertOne } from "../../middleware/database/db-connect";
+import { dbConnect, insertOne, updateOne } from "../../middleware/database/db-connect";
 import mongoose from "mongoose";
 import { USERS_COLLECT } from "../../middleware/database/db-users";
 
-const TEST_EMAIL = 'qrst@gmail.com'
-const TEST_USRNM = 'qrst'
-const TEST_PW = 'cdfg'
+const TEST_EMAIL = 'qrst@gmail.com';
+const TEST_USRNM = 'qrst';
+const TEST_NEW_USRNM = 'uvwx'; 
+const TEST_PW = 'cdfg';
 
 
 describe('tests', async () => {
@@ -28,7 +29,7 @@ describe('tests', async () => {
             'email': TEST_EMAIL,
             'username': TEST_USRNM,
             'password': TEST_PW
-        }
+        };
 
         await insertOne(
             USERS_COLLECT,
@@ -36,11 +37,43 @@ describe('tests', async () => {
             filt
         );
 
+        // make sure the insert worked
         const my_model = mongoose.model(USERS_COLLECT, userSchema);
-        let search = await my_model.findOne(filt).exec();
+        let search = await my_model.findOne(filt);
         assert(search !== null);
-        await my_model.findOneAndDelete(filt).exec();
+
+        // cleanup
+        await my_model.findOneAndDelete(filt);
     });
-}); 
 
 
+    test('updateOne', async () => {
+        let filt = {
+            'email': TEST_EMAIL,
+            'username': TEST_USRNM,
+            'password': TEST_PW
+        };
+
+        // create the new document
+        const my_model = mongoose.model(USERS_COLLECT, userSchema);
+        const doc = new my_model(filt);
+        await doc.save();
+
+
+        await updateOne(
+            USERS_COLLECT,
+            userSchema,
+            filt,
+            {'username': TEST_NEW_USRNM}
+        );
+
+        filt['username'] = TEST_NEW_USRNM;
+
+        // make sure the update worked
+        let search = await my_model.findOne(filt);
+        assert(search !== null);
+
+        // cleanup
+        await my_model.findOneAndDelete(filt);
+    });
+});
