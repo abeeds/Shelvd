1f87409d74d72f8d1e1663f7942c754db4e4b64d
Shahria Abeed <shahriaabeed@gmail.com>
2024-07-28 14:28:40 -0400

    created db-shelves test file, wrote test for insertShelf

---
+++ server/src/tests/database/db-shelves.test.ts
@@ -0,0 +1,46 @@
+import assert from "node:assert";
+import test from "node:test";
+import mongoose, { mongo } from "mongoose";
+import * as argon2 from "argon2";
+import { after, before, describe } from "node:test";
+import { dbConnect } from "../../middleware/database/db-connect";
+import { insertShelf, SHELF_COLLECT } from "../../middleware/database/db-shelves";
+import { shelfSchema } from "../../middleware/database/schemas/shelf";
+require('dotenv').config();
+
+
+const SHELF_MODEL = mongoose.model(SHELF_COLLECT, shelfSchema);
+const TEST_OWNER = 'Jasnah Kholin';
+const TEST_NAME = 'History';
+const TEST_DESCRIPTION = 'Aharietiam';
+
+
+describe('db-shelves', async() => {
+    before(async () => {
+        await dbConnect();
+    });
+
+
+    after(() => {
+        mongoose.disconnect();
+    });
+
+
+    test('insertShelf', async () => {
+        const filt = {
+            owner: TEST_OWNER,
+            name: TEST_NAME,
+            description: TEST_DESCRIPTION
+        }
+
+        await insertShelf(
+            TEST_OWNER,
+            TEST_NAME,
+            TEST_DESCRIPTION
+        );
+
+        let search = await SHELF_MODEL.findOne(filt);
+        assert(search !== null);
+        await SHELF_MODEL.findOneAndDelete(filt);
+    });
+});
