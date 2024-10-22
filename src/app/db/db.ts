2b3beeec59ab81b640286e6065dea9b971343d58
Shahria Abeed <shahriaabeed@gmail.com>
2024-10-22 19:53:47 -0400

    wrote tableExists, changed attribute names

--- src/app/db/db.ts
+++ src/app/db/db.ts
@@ -6,30 +6,30 @@ export let DB: any;
 
 // Table names and attributes
 const ITEM = "Item";
-const ITEMID = "itemID";
-const ITEMNAME = "itemNAME";
-const ITEMIMAGE = "itemImage"; // url to image
-const ITEMTYPE = "itemType"; // direct to another table
+const ITEMID = "item_id";
+const ITEMNAME = "item_name";
+const ITEMIMAGE = "item_img"; // url to image
+const ITEMTYPE = "item_type"; // direct to another table
 
 const SHELF = "Shelf";
-const SHELFID = "shelfID";
-const SHELFNAME = "shelfName";
-const SHELFDESC = "shelfDesc";
+const SHELFID = "shelf_id";
+const SHELFNAME = "shelf_name";
+const SHELFDESC = "shelf_desc";
 
-const SUBSHELF = "SubShelf";
-const PARENTID = "parentID";
-const CHILDID = "childID";
+const SUBSHELF = "Subshelf";
+const PARENTID = "parent_id";
+const CHILDID = "child_id";
 
 const SHELFITEM = "ShelfItem"; // shelfID and parentID
 
 const TYPE = "Type"; // table for different item types
-const TYPEID = "typeID";
-const TYPENAME = "typeName";
+const TYPEID = "type_id";
+const TYPENAME = "type_name";
 
-const TABLECOUNTS = "TableCounts";
-const TCTABLENAME = "tcTableName";
-const TCROWCOUNT = "tcRowCount";
-const TCLASTID = "tcLastUsedID";
+const TABLECOUNT = "TableCount";
+const TCTABLENAME = "tc_table_name";
+const TCROWCOUNT = "tc_row_count";
+const TCLASTID = "tc_last_id";
 
 
 export function numTables(): Promise<number> {
@@ -55,6 +55,16 @@ export function createTable(name: string, column: string) {
 }
 
 
+export function tableExists(name: string): Promise<Boolean> {
+    return new Promise((resolve) => {
+        DB.get(`SELECT name FROM sqlite_master WHERE type='table' AND name='${name}'`, (err: any, row: any) => {
+            if (row) resolve(true);
+            else resolve(false);
+        });
+    });
+}
+
+
 export function getColumns(table_name: string): Promise<any[]> {
     const query = `PRAGMA table_info(${table_name})`;
     let res: any[] = [];
@@ -73,45 +83,48 @@ export function getColumns(table_name: string): Promise<any[]> {
 
 // creates the tables required for this app to work
 export function initTables() {
-    createTable(SHELF,
-        `${SHELFID} int,
-        ${SHELFNAME} varchar(255),
-        ${SHELFDESC} TEXT,
-        PRIMARY KEY (${SHELFID})`
-    );
-    createTable(TYPE,
-        `${TYPEID} int,
-        ${TYPENAME} varchar(255),
-        PRIMARY KEY (${TYPEID})`
-    )
-    createTable(ITEM,
-        `${ITEMID} int,
-        ${ITEMNAME} TEXT,
-        ${ITEMIMAGE} TEXT,
-        ${ITEMTYPE} int,
-        PRIMARY KEY (${ITEMID}),
-        FOREIGN KEY (${ITEMTYPE}) REFERENCES ${TYPE}(${TYPEID})`
-    );
-    createTable(SUBSHELF,
-        `${PARENTID} int,
-        ${CHILDID} int,
-        PRIMARY KEY (${PARENTID}, ${CHILDID}),
-        FOREIGN KEY (${PARENTID}) REFERENCES ${SHELF}(${SHELFID}),
-        FOREIGN KEY (${CHILDID}) REFERENCES ${SHELF}(${SHELFID})`
-    );
-    createTable(SHELFITEM,
-        `${SHELFID} int,
-        ${ITEMID} int,
-        PRIMARY KEY (${SHELFID}, ${ITEMID}),
-        FOREIGN KEY (${SHELFID}) REFERENCES ${SHELF}(${SHELFID}),
-        FOREIGN KEY (${ITEMID}) REFERENCES ${ITEM}(${ITEMID})`
-    );
-    createTable(TABLECOUNTS,
-        `${TCTABLENAME} varchar(255),
-        ${TCROWCOUNT} int,
-        ${TCLASTID} int,
-        PRIMARY KEY (${TCTABLENAME})`
-    );
+    // add a row into TC for each of these
+    DB.serialize(() => {
+        createTable(TABLECOUNT,
+            `${TCTABLENAME} varchar(255),
+            ${TCROWCOUNT} int,
+            ${TCLASTID} int,
+            PRIMARY KEY (${TCTABLENAME})`
+        );
+        createTable(SHELF,
+            `${SHELFID} int,
+            ${SHELFNAME} varchar(255),
+            ${SHELFDESC} TEXT,
+            PRIMARY KEY (${SHELFID})`
+        );
+        createTable(TYPE,
+            `${TYPEID} int,
+            ${TYPENAME} varchar(255),
+            PRIMARY KEY (${TYPEID})`
+        )
+        createTable(ITEM,
+            `${ITEMID} int,
+            ${ITEMNAME} TEXT,
+            ${ITEMIMAGE} TEXT,
+            ${ITEMTYPE} int,
+            PRIMARY KEY (${ITEMID}),
+            FOREIGN KEY (${ITEMTYPE}) REFERENCES ${TYPE}(${TYPEID})`
+        );
+        createTable(SUBSHELF,
+            `${PARENTID} int,
+            ${CHILDID} int,
+            PRIMARY KEY (${PARENTID}, ${CHILDID}),
+            FOREIGN KEY (${PARENTID}) REFERENCES ${SHELF}(${SHELFID}),
+            FOREIGN KEY (${CHILDID}) REFERENCES ${SHELF}(${SHELFID})`
+        );
+        createTable(SHELFITEM,
+            `${SHELFID} int,
+            ${ITEMID} int,
+            PRIMARY KEY (${SHELFID}, ${ITEMID}),
+            FOREIGN KEY (${SHELFID}) REFERENCES ${SHELF}(${SHELFID}),
+            FOREIGN KEY (${ITEMID}) REFERENCES ${ITEM}(${ITEMID})`
+        );
+    });
 }
 
 
