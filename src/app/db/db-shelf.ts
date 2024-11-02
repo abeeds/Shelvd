2e2a90a882668411619bad551f5861f462013e2a
Shahria Abeed <shahriaabeed@gmail.com>
2024-11-02 12:19:00 -0400

    wrote checkSubshelf

--- src/app/db/db-shelf.ts
+++ src/app/db/db-shelf.ts
@@ -74,6 +74,20 @@ export async function deleteShelf(shelf_id: number) {
 
 
 
+export async function checkSubshelf(parent_id: number, child_id: number) {
+    const exists = await tableExists(SHELF);
+    if(!exists) return [false, "Shelf table does not exist."];
+
+    let res = [false, `${child_id} is not a child of ${parent_id}.`]
+    await DB.run(`SELECT * FROM ${SUBSHELF} WHERE ${PARENTID} = ? AND ${CHILDID} = ?`,
+        [parent_id, child_id], (err: any, row: any) => {
+            if (err) res = [false, "Failed to check subshelf."];
+            else if (row) res = [true, `${child_id} is a child of ${parent_id}.`]
+        }
+    );
+    return res;    
+}
+
 
 // add child shelf
 // create subshelf relation
