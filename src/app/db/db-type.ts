6238400e3eea404eea7004284af01cf05b056e74
Shahria Abeed <shahriaabeed@gmail.com>
2024-11-10 12:11:32 -0500

    wrote insertType

--- src/app/db/db-type.ts
+++ src/app/db/db-type.ts
@@ -8,6 +8,17 @@ const TYPE_COLS = `(${TYPENAME})`;
 export async function insertType(type_name: string): Promise<[boolean, string]> {
     const exists = await tableExists(TYPE);
     if(!exists) return [false, "Type table does not exist."];
+
+    return new Promise((resolve) => {
+        DB.run(`INSERT INTO ${TYPE} ${TYPE_COLS}
+            VALUES (?)`,
+            [type_name],
+            (err: any) => {
+                if (err) resolve([false, `${err}`]);
+                resolve([true, "Type created successfully."]);
+            }
+        );
+    });
 }
 
 
