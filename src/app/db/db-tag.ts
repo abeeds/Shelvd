0e0a620a70f000f0188a2378f9643ee3853b483e
Shahria Abeed <shahriaabeed@gmail.com>
2024-11-18 11:34:13 -0500

    wrote insertTag

--- src/app/db/db-tag.ts
+++ src/app/db/db-tag.ts
@@ -1,11 +1,25 @@
-import { DB } from "./db";
+import { DB, tableExists } from "./db";
 import { TAG, TAGNAME } from "./db"; // tag columns
 
 
 const TAG_COLS = `(${TAGNAME})`;
 
 
-// create tag
+export async function insertTag(tag_name: string): Promise<[boolean, string]> {
+    const exists = await tableExists(TAG);
+    if(!exists) return [false, "Tag table does not exist."];
+
+    return new Promise((resolve) => {
+        DB.run(`INSERT INTO ${TAG} ${TAG_COLS}
+            VALUES (?)`,
+            [TAGNAME],
+            (err: any) => {
+                if (err) resolve([false, `${err}`]);
+                resolve([true, "Tag created successfully."]);
+            }
+        );
+    });
+}
 
 
 // rename tag
