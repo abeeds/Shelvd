ee8565f9f1b2c2bc98c33a8b399b02a1d1d27b3d
Shahria Abeed <shahriaabeed@gmail.com>
2024-11-18 11:43:10 -0500

    wrote updateTag

--- src/app/db/db-tag.ts
+++ src/app/db/db-tag.ts
@@ -1,5 +1,5 @@
 import { DB, tableExists } from "./db";
-import { TAG, TAGNAME } from "./db"; // tag columns
+import { TAG, TAGID, TAGNAME } from "./db"; // tag columns
 
 
 const TAG_COLS = `(${TAGNAME})`;
@@ -22,7 +22,20 @@ export async function insertTag(tag_name: string): Promise<[boolean, string]> {
 }
 
 
-// rename tag
+export async function updateTag(id: number, new_name: string) {
+    const exists = await tableExists(TAG);
+    if(!exists) return [false, "Item table does not exist"];
+
+    return new Promise((resolve) => {
+        DB.run(
+            `UPDATE ${TAG} SET ${TAGNAME} = ? WHERE ${TAGID} = ?`,
+            [new_name, id],
+            (err: any) => {
+                if (err) resolve([false, `${err}`]);
+                resolve([true, `Tag ${id} renamed to ${new_name} successfully.`]);
+        });
+    });
+}
 
 
 // delete tag
