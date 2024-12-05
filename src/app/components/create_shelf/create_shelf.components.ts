1d777c1c99638d18ac4c95e5889de5d889a1cd5f
Shahria Abeed <shahriaabeed@gmail.com>
2024-12-05 10:58:02 -0500

    [Frontend] Using form control in create shelf form

--- src/app/components/create_shelf/create_shelf.components.ts
+++ src/app/components/create_shelf/create_shelf.components.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
     selector: 'create-shelf',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, ReactiveFormsModule],
     templateUrl: `./create_shelf.component.html`,
     styleUrl: `./create_shelf.component.css`
 }) export class CreateShelfComponent {
-    
+
+    shelf_name = new FormControl('')
+    shelf_desc = new FormControl('')
 }
