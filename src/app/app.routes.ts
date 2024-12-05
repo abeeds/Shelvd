dd8e30241c594c915985e4600fa9e16071fd8efa
Shahria Abeed <shahriaabeed@gmail.com>
2024-12-05 09:30:15 -0500

    created new route for inserting shelf and initialized component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DevComponent } from './components/dev/dev.component';
+import { CreateShelfComponent } from './components/create_shelf/create_shelf.components';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'dev', component:  DevComponent},
+    {path: 'create_shelf', component: CreateShelfComponent}
     // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
---
+++ src/app/components/create_shelf/create_shelf.components.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+    selector: 'create-shelf',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: `./create_shelf.component.html`,
+    styleUrl: `./create_shelf.component.css`
+}) export class CreateShelfComponent {
+    
+}
